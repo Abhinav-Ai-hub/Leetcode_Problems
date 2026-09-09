d9b7593d04ac46569468ae7d8dd3a4fc3c655afa
Abhinav Srivastava <abhinavsrivastava820@gmail.com>
2026-09-09 23:13:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,24 +1,23 @@
-
-
-//top down with memo[]
 class Solution {
-
-    public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        Arrays.fill(memo, -1);
-
-        return climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
-    }
-
-    private int climbStairs(int n, int[] memo) {
-        if (n < 0) return 0;
-        if (n == 0 || n == 1) {
-            memo[n] = 1;
-            return memo[n];
+    Map <Integer,Integer> map=new HashMap<>();
+    public int climbStairsHelper(int n){
+        if(map.containsKey(n)){
+            return map.get(n);
         }
-        if (memo[n] != -1) return memo[n];
+        if(n==0){
+            return 1;
+            
+        }
+        if(n<0){
+            return 0;
+        }
+        int res=climbStairsHelper(n-1)+climbStairsHelper(n-2);
+        map.put(n,res);
+        return res;
+    }
+    public int climbStairs(int n) {
+        return climbStairsHelper(n);
 
-        memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
-        return memo[n];
+
     }
 }
