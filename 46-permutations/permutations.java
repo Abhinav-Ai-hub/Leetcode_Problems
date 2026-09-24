6fc869a9689a97c6970b6f890fc74ee46fd36689
Abhinav Srivastava <abhinavsrivastava820@gmail.com>
2026-09-24 18:43:09 +0530

    Time: 1 ms (88.17%) | Memory: 45.3 MB (78.73%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,51 @@
 class Solution {
-    private void recurPermute(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean []freq) {
-        if(ds.size() == nums.length) {
+
+    private void recurPermute(
+        int[] nums,
+        int i,
+        List<Integer> ds,
+        List<List<Integer>> ans,
+        boolean[] freq
+    ) {
+
+        // We have checked all elements for this level
+        if (i == nums.length) {
+            return;
+        }
+
+        // Complete permutation
+        if (ds.size() == nums.length) {
             ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i = 0; i < nums.length; i++) {
-            if(!freq[i]) {// we are checking if the index element has already been used or free to be used
-                freq[i] = true;// here we are declaring that yes now the elemtn at the endex i is picked
-                ds.add(nums[i]);
-                recurPermute(nums, ds, ans, freq);
-                ds.remove(ds.size() - 1);
-                freq[i] = false;// declaring that while backtracking since we removed the last element so now that elemnent can be used thatswhy in map we turn it to false again so that by condition checking it becomes true and enters the if clause
-            }
+
+        // Choice: nums[i] is free
+        if (!freq[i]) {
+
+            freq[i] = true;
+            ds.add(nums[i]);
+
+            // Move to the next recursion level
+            recurPermute(nums, 0, ds, ans, freq);
+
+            // Backtrack
+            ds.remove(ds.size() - 1);
+            freq[i] = false;
         }
+
+        // Instead of the for loop:
+        // i++ becomes another recursive call
+        recurPermute(nums, i + 1, ds, ans, freq);
     }
+
     public List<List<Integer>> permute(int[] nums) {
+
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length];
-        recurPermute(nums, ds, ans, freq);
+        boolean[] freq = new boolean[nums.length];
+
+        recurPermute(nums, 0, ds, ans, freq);
+
         return ans;
     }
 }
